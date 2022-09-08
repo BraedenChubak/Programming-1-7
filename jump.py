9b02d230dd8b395c65781eb47006bb5f787ea8e7
Braeden Chubak <112727144+BraedenChubak@users.noreply.github.com>
2022-09-08 19:31:11 +0000

    Added Karel files + finished jump.py

---
+++ jump.py
@@ -0,0 +1,30 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+    move()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
---
+++ kareltemplate.py
@@ -0,0 +1,11 @@
+from stanfordkarel import *
+
+
+def main():
+    """ Karel code goes here! """
+    
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
