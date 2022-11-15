637e5faeeba10a66c29cc7484d40369c0930ef8d
Braeden Chubak <112727144+BraedenChubak@users.noreply.github.com>
2022-11-15 19:58:28 +0000

    stringstuff finished

---
+++ stringstuff.py
@@ -0,0 +1,10 @@
+thing = "HELLO world HERE is SOME text"
+print(thing.lower())
+print(thing.upper())
+print(thing.isdigit())
+locworld = thing.find("world")
+print(locworld)
+print(thing[::-1])
+print("Is palindrome?", thing == thing[::-1])
+print(thing.split(' '))
+print("Hello " * 5)
