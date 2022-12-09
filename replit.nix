266d29c46e3cbfc484fd4789b7bcda4d9bf95331
Braeden Chubak <112727144+BraedenChubak@users.noreply.github.com>
2022-12-09 20:35:20 +0000

    finished snake

--- replit.nix
+++ replit.nix
@@ -1,18 +1,16 @@
 { pkgs }: {
-  deps = [
-    pkgs.python38Full
-  ];
+	deps = [
+		pkgs.python38Full
+	];
   env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      # Needed for pandas / numpy
+    LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath ([
+      # Neded for pandas / numpy
       pkgs.stdenv.cc.cc.lib
       pkgs.zlib
       # Needed for pygame
       pkgs.glib
-      # Needed for matplotlib
-      pkgs.xorg.libX11
-    ];
+    ] ++ (with pkgs.xlibs; [ libX11 libXext libXinerama libXcursor libXrandr libXi libXxf86vm ]));
+
     PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
-    LANG = "en_US.UTF-8";
   };
 }
